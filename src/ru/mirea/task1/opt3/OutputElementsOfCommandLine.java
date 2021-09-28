0773051b53961923f1a56ac193004e2eff9d1a31
chursinov <chursinov98@gmail.com>
2021-09-28 19:45:58 +0300

    Added task1->opt3

---
+++ src/ru/mirea/task1/opt3/OutputElementsOfCommandLine.java
@@ -0,0 +1,9 @@
+package ru.mirea.task1.opt3;
+
+class OutputElementsOfCommandLine {
+    public static void main(String[] args){
+        for (int i = 0;i < args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
