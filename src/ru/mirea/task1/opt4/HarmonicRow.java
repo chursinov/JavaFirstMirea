b9eb6b293ea57a874a29a60d0bd25d191d00bb5d
chursinov <chursinov98@gmail.com>
2021-09-28 20:04:44 +0300

    Added task1->opt4

---
+++ src/ru/mirea/task1/opt4/HarmonicRow.java
@@ -0,0 +1,12 @@
+package ru.mirea.task1.opt4;
+
+public class HarmonicRow {
+    public static void main(String[] args){
+        for (int i = 1; i <= 10;i++){
+            float j = i;
+            float m = 1/j;
+            System.out.printf("%f",m);
+            System.out.println();
+        }
+    }
+}
