16bf324cfadd21098bc340b4a20fdc427d2f2b70
chursinov <chursinov98@gmail.com>
2021-09-29 10:43:44 +0300

    Added task3->opt2

---
+++ src/ru/mirea/task3/opt2/TestHuman.java
@@ -0,0 +1,17 @@
+package ru.mirea.task3.opt2;
+
+public class TestHuman {
+    public static void main(String[] args){
+        Human c1 = new Human(21,"Artemiy", 'M');
+        Hand c2 = new Hand (21,"Artemiy",'M',32,"Black");
+        Hand c3 = new Hand (21,"Artemiy",'M',32,"Black");
+        Leg c4 = new Leg (21,"Artemiy",'M',56,"White");
+        Leg c5 = new Leg (21,"Artemiy",'M',56,"White");
+        c2.Summary();
+        c3.Summary();
+        c4.Summary();
+        c5.Summary();
+        c1.Summary();
+    }
+
+}
